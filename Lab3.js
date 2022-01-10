87ebe94e7dc4295e28a7b1be527b1e0d45775fca
blackgirlwhocodes <84105697+blackgirlwhocodes@users.noreply.github.com>
2022-01-10 18:33:57 -0500

    Add files via upload

---
+++ Lab3.js
@@ -0,0 +1,71 @@
+const submissions = [
+    {
+        name: 'Jane',
+        score: 95,
+        date: '2020/01/24',
+        passed: true,
+     }
+     ,
+    {name: 'Joe',
+    score: 77,
+    date: '2018/05/15',
+    passed: true,}
+,
+{
+    name: 'Jack', 
+    score: 59,
+    date: 2019/07/05,
+    passed: false,
+}
+,
+{
+    name: 'Jill', 
+    score: 88, 
+    date: '202/04/22',
+    passed: true,
+}
+]
+
+function addSubmission (array, newName, newScore, newDate);{
+    const newStudent = {
+        name: newName,
+        score: newScore,
+        date: newDate,
+        passed: newScore >= 60,
+    }
+array.push(newStudent);
+    
+
+
+}
+addSubmission (submissions,'Valerie',86, '2021/12/12')
+
+
+function deleteSubmissionByIndex (array, index) {
+array.splice (index,1);
+}
+;
+
+function deleteSubmissionByName (array,name) {
+array.splice ();
+}
+
+;
+
+
+function editSubmission(array, index, score){  
+    0 => 60
+
+}
+
+function findSubmissionByName(array, name) {
+submissions.findIndex()
+;}
+
+function findLowestScore (array) {
+
+}
+
+function findAverageScore (array) {
+
+}
